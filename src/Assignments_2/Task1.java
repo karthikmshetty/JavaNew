016d7a544db236dfab4b0e27e13007dac5308d57
karthikmshetty <karthikmshetty108@gmail.com>
2023-08-05 09:40:24 +0530

    .

--- src/Assignments_2/Task1.java
+++ src/Assignments_2/Task1.java
@@ -1,15 +1,18 @@
 package Assignments_2;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Random;
+
+import collections.Arraylist;
 
 public class Task1 {
 
-	public static void main(String[] args) 
-	{
-					
-		
-		
-		
-	}
+	
+	
+	
+	
+	
 
 }
--- src/naveen/Task7.java
+++ src/naveen/Task7.java
@@ -10,48 +10,53 @@ import java.util.Random;
 
 public class Task7 {
 
-	    private static final String lowerCase = "abcdefghijklmnopqrstuvwxyz";
-	    private static final String upperCase= lowerCase.toUpperCase();
-	    private static final String digits = "0123456789";
-	    private static final String specialChars = "!@#$%^&*()_-+=<>?";
+	public static void main(String[] args) 
+	{
+	   String lowerCase = "abcdefghijklmnopqrstuvwxyz";
+	   String upperCase= lowerCase.toUpperCase();
+	   String digits = "0123456789";
+	   String specialChars = "!@#$%^&*()_-+=<>?";	
+	   int len=10,count=0;
+	   String output="";
+	  
+	   Random ran = new Random();
+	   
+	   while(count++<10)
+	   {
+	   ArrayList<Character> list = new ArrayList();
+	   list.add(generatePassword(lowerCase,ran));
+	   list.add(generatePassword(upperCase,ran));
+	   list.add(generatePassword(digits,ran));
+	   list.add(generatePassword(specialChars,ran));
+	   
+	   String allChar=lowerCase+upperCase+digits+specialChars;
+	   for(int i=5;i<=len;i++)
+	   {
+		   list.add(generatePassword(allChar,ran));
+	   }
+	   
+        // Shuffle the password characters
+	   Collections.shuffle(list);
+	   
+	   //build the final password
+	   for(char c:list)
+	   {
+		   output+=c;     //we can print list directly but the o/p will be surrounded with [ ]
+	   }
+	   
+	   System.out.println(output);
+	   output="";
 
-	    public static void main(String[] args) {
-	        int length = 10; // Desired password length
-	        int n=1;
- 	        String output="";
+	   }    
+		
+	}
+	
+	//To generate password randomly according to given set of string values
+	public static char generatePassword(String value,Random ran)
+	{
+		int ranNum=ran.nextInt(value.length());
+		return value.charAt(ranNum);
+		
+	}	    
 
-	        while(n++<10)
-	        {
-	        	 Random random = new Random();
-	 	        List<Character> passwordCharacters = new ArrayList<>();
-
-	 	        passwordCharacters.add(getRandomCharacter(lowerCase, random));
-	 	        passwordCharacters.add(getRandomCharacter(upperCase, random));
-	 	        passwordCharacters.add(getRandomCharacter(digits, random));
-	 	        passwordCharacters.add(getRandomCharacter(specialChars, random));
-
-	 	        // Fill the remaining length with random characters
-	 	        for (int i = 4; i < length; i++) {
-	 	            String allChars = lowerCase + upperCase + digits + specialChars;
-	 	            passwordCharacters.add(getRandomCharacter(allChars, random));
-	 	        }
-
-	 	        // Shuffle the password characters
-	 	        Collections.shuffle(passwordCharacters);
-
-	 	        // Build the final password string
-	 	       for (char c : passwordCharacters) {
-	 	            output+=c;
-	        }
-		        System.out.println("Generated Password:  " + output);
-		        output="";
-   
-	        }
-    
-	    }
-
-	    private static char getRandomCharacter(String characterSet, Random random) {
-	        int randomIndex = random.nextInt(characterSet.length());
-	        return characterSet.charAt(randomIndex);
-	    }
 }
