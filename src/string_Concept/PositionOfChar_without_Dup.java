5a5dfe6abe5c1921c4c5d03fc6dc70395f01a56c
karthikmshetty <135118312+karthikmshetty@users.noreply.github.com>
2025-03-12 07:31:08 +0530

    Update PositionOfChar_without_Dup.java

--- src/string_Concept/PositionOfChar_without_Dup.java
+++ src/string_Concept/PositionOfChar_without_Dup.java
@@ -5,31 +5,17 @@ import java.util.LinkedHashSet;
 public class PositionOfChar_without_Dup {
 
 	public static void main(String[] args) {
-		String s = "Tester";
-		String s1 = s.toLowerCase();
-		
-		LinkedHashSet<Character> set = new LinkedHashSet<Character>();
-		for(int i=0;i<s1.length();i++)
-		{
-			set.add(s1.charAt(i));
-			}
-		
-		for(char ch:set)
-		{
 			
-			for(int i=0;i<s1.length();i++)
-			{
-				
-				if(ch==s1.charAt(i))
-				{
-					System.out.println(ch+" is present in position of "+i);
-					break;
-				}
-				
-			}
-			
-			
-		}
+	String s = "Tester";
+        String s1 = s.toLowerCase();
+
+        Map<Character, Integer> charPositionMap = new LinkedHashMap<>();
+
+        for (int i = 0; i < s1.length(); i++) {
+            charPositionMap.putIfAbsent(s1.charAt(i), i);
+        }
+
+        charPositionMap.forEach((ch, pos) -> System.out.println(ch + " is present in position of " + pos));
 		
 		
 	}
